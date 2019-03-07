991c1a6af6de7714e8f63df0bd4a645b243c5beb
Thomas Roberts <t.92.roberts@gmail.com>
2019-03-07 13:16:34 +0100

    Added MIP starts

--- Agile Sprint Planning/agile.cpp
+++ Agile Sprint Planning/agile.cpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <tuple>
 #include <chrono>
+#include <map>
 ILOSTLBEGIN
 
 using namespace std;
@@ -13,6 +14,7 @@ class Story {
 public:
 	int storyNumber, businessValue, storyPoints;
 	vector<int> dependencies;
+	vector<int> dependees;
 
 	Story() {};
 
@@ -34,26 +36,56 @@ public:
 			string dependenciesString = "";
 
 			for (int i = 0; i < this->dependencies.size(); ++i) {
-				if (i == 0) {
+				if (i == 0)
 					dependenciesString += "Story " + to_string(this->dependencies[i]);
-				}
-				else {
+				else
 					dependenciesString += ", Story " + to_string(this->dependencies[i]);
-				}
 			}
 
 			return dependenciesString;
 		}
-		else {
+		else
 			return "None";
+	}
+
+	string printDependees() {
+		if (this->dependees.size() > 0) {
+			string dependeesString = "";
+
+			for (int i = 0; i < this->dependees.size(); ++i) {
+				if (i == 0)
+					dependeesString += "Story " + to_string(this->dependees[i]);
+				else
+					dependeesString += ", Story " + to_string(this->dependees[i]);
+			}
+
+			return dependeesString;
 		}
+		else
+			return "None";
+	}
+
+	bool operator == (const Story& other) const {
+		return this->storyNumber == other.storyNumber;
+	}
+
+	bool operator != (const Story& other) const {
+		return this->storyNumber != other.storyNumber;
+	}
+
+	bool operator < (const Story& other) const {
+		return this->storyNumber < other.storyNumber;
+	}
+
+	bool operator <= (const Story& other) const {
+		return this->storyNumber <= other.storyNumber;
 	}
 
 	string toString() {
 		return "Story " + to_string(storyNumber)
 			+ " (business value: " + to_string(businessValue)
-			+ ", story points: " + to_string(storyPoints)
-			+ ", dependencies: " + printDependencies() + ")";
+			+ " | story points: " + to_string(storyPoints)
+			+ " | dependencies: " + printDependencies() + ")";
 	}
 };
 
@@ -69,8 +101,31 @@ public:
 		this->sprintBonus = bonus;
 	}
 
+	bool withinCapacity(int storyPoints) {
+		return storyPoints <= this->sprintCapacity;
+	}
+
+	bool operator == (const Sprint& other) const {
+		return this->sprintNumber == other.sprintNumber;
+	}
+
+	bool operator < (const Sprint& other) const {
+		return this->sprintNumber < other.sprintNumber;
+	}
+
+	bool operator <= (const Sprint& other) const {
+		return this->sprintNumber <= other.sprintNumber;
+	}
+
 	string toString() {
-		return ">> Sprint " + to_string(sprintNumber) +
+		string sprint;
+
+		if (sprintNumber != -1)
+			sprint = ">> Sprint " + to_string(sprintNumber);
+		else
+			sprint = ">> Product Backlog";
+
+		return sprint +
 			" (capacity: " + to_string(sprintCapacity) +
 			", bonus: " + to_string(sprintBonus) + ")";
 	}
@@ -97,6 +152,216 @@ public:
 			return true;
 		else
 			return false;
+	}
+};
+
+class Roadmap {
+public:
+	vector<Story> stories;
+	vector<Sprint> sprints;
+
+	map<Story, Sprint> storyToSprint;
+	map<Sprint, vector<Story>> sprintToStories;
+
+	Roadmap() {};
+
+	Roadmap(vector<Story> stories, vector<Sprint> sprints) {
+		this->stories = stories;
+		this->sprints = sprints;
+	}
+
+	bool validInsert(Story story, Sprint sprint) {
+		// Check if adding the story overloads the sprint
+		if (story.storyPoints + storyPointsAssignedToSprint(sprint) > sprint.sprintCapacity && sprint.sprintNumber != -1)
+			return false;
+
+		// Check that no dependees are assigned earlier/same as the sprint
+		for (int dependeeNumber : story.dependees) {
+			Story dependee = stories[dependeeNumber];
+
+			auto it = storyToSprint.find(dependee);
+
+			// The dependee is assigned somewhere
+			if (it != storyToSprint.end()) {
+				Sprint dependeeAssignedSprint = it->second;
+
+				// The dependee is assigned earlier/same as this sprint
+				if (dependeeAssignedSprint.sprintNumber <= sprint.sprintNumber && dependeeAssignedSprint.sprintNumber != -1)
+					return false;
+			}
+		}
+
+		// Check that each of the story's dependencies are assigned before the sprint
+		for (int dependencyNumber : story.dependencies) {
+			Story dependency = stories[dependencyNumber];
+
+			auto it = storyToSprint.find(dependency);
+
+			// Only stories that are assigned to a sprint are in the map
+			if (it == storyToSprint.end())
+				// The dependee isn't assigned to a sprint
+				return false;
+
+			Sprint dependencyAssignedSprint = it->second;
+
+			// The dependee is assigned to the product backlog
+			if (dependencyAssignedSprint.sprintNumber == -1)
+				return false;
+
+			// The story is assigned to an earlier sprint than its dependee
+			if (sprint.sprintNumber <= dependencyAssignedSprint.sprintNumber)
+				return false;
+		}
+
+		// The sprint doesn't get overloaded and the story's dependencies are satisfied
+		return true;
+	}
+
+	int storyPointsAssignedToSprint(Sprint sprint) {
+		vector<Story> sprintStories = sprintToStories[sprint];
+
+		// Sum of the story points of all the stories assigned to the sprint
+		int assignedStoryPoints = 0;
+
+		for (Story story : sprintStories)
+			assignedStoryPoints += story.storyPoints;
+
+		return assignedStoryPoints;
+	}
+
+	void addStoryToSprint(Story story, Sprint sprint) {
+		storyToSprint[story] = sprint;
+		sprintToStories[sprint].push_back(story);
+	}
+
+	void removeStoryFromSprint(Story story, Sprint sprint) {
+		storyToSprint.erase(storyToSprint.find(story));
+		sprintToStories[sprint].erase(remove(sprintToStories[sprint].begin(), sprintToStories[sprint].end(), story), sprintToStories[sprint].end());
+	}
+
+	void moveStory(Story story, Sprint from, Sprint to) {
+		removeStoryFromSprint(story, from);
+		addStoryToSprint(story, to);
+	}
+
+	int calculateValue() {
+		int totalValue = 0;
+
+		for (pair<Story, Sprint> pair : storyToSprint) {
+			Story story = pair.first;
+			Sprint sprint = pair.second;
+
+			// Don't add value from stories assigned to the product backlog
+			if (sprint.sprintNumber != -1)
+				totalValue += story.businessValue * sprint.sprintBonus;
+		}
+
+		return totalValue;
+	}
+
+	bool sprintCapacitiesSatisifed() {
+		for (pair<Sprint, vector<Story>> pair : sprintToStories) {
+			Sprint sprint = pair.first;
+
+			// Don't check the capacity of the product backlog
+			if (sprint.sprintNumber != -1) {
+				int assignedStoryPoints = storyPointsAssignedToSprint(sprint);
+
+				// Check if the sprint is overloaded
+				if (!sprint.withinCapacity(assignedStoryPoints))
+					// The story points assigned are not within the sprint's capacity
+					return false;
+			}
+		}
+
+		// All sprints are within capacity
+		return true;
+	}
+
+	bool storyDependenciesSatisfied() {
+		for (pair<Story, Sprint> assignment : storyToSprint) {
+			Story story = assignment.first;
+			Sprint assignedSprint = assignment.second;
+
+			// Don't check stories assigned to the product backlog
+			if (assignedSprint.sprintNumber != -1) {
+				for (int dependeeNumber : story.dependencies) {
+					Story dependee = stories[dependeeNumber];
+					Sprint dependeeAssignedSprint = storyToSprint[dependee];
+
+					// Only stories that are assigned to a sprint are in the map
+					if (storyToSprint.find(dependee) == storyToSprint.end()) {
+						// The dependee isn't assigned to a sprint
+						return false;
+					}
+					else if (dependeeAssignedSprint.sprintNumber == -1) {
+						// The dependee is assigned to the special product backlog
+						return false;
+					}
+					else {
+						// Check where the story is assigned compared to its dependee
+						if (assignedSprint <= dependeeAssignedSprint)
+							// The story is assigned to an earlier sprint than its dependee
+							return false;
+					}
+				}
+			}
+		}
+
+		// All stories have their dependees assigned to an earlier sprint
+		return true;
+	}
+
+	bool isFeasible() {
+		return sprintCapacitiesSatisifed() && storyDependenciesSatisfied();
+	}
+
+	string printStoryRoadmap() {
+		string outputString = "";
+
+		for (pair<Story, Sprint> pair : storyToSprint) {
+			Story story = pair.first;
+			Sprint sprint = pair.second;
+
+			outputString += story.toString() + "\n  >> " + sprint.toString() + "\n";
+		}
+
+		return outputString;
+	}
+
+	string printSprintRoadmap() {
+		string outputString = "";
+
+		for (Sprint sprint : sprints) {
+			if (sprint.sprintNumber == -1)
+				outputString += "Product Backlog";
+			else
+				outputString += sprint.toString();
+
+			vector<Story> sprintStories = sprintToStories[sprint];
+			int valueDelivered = 0;
+			int storyPointsAssigned = 0;
+
+			if (sprintStories.empty()) {
+				outputString += "\n\tNone";
+			}
+			else {
+				for (Story story : sprintStories) {
+					valueDelivered += story.businessValue;
+					storyPointsAssigned += story.storyPoints;
+
+					outputString += "\n\t" + story.toString();
+				}
+			}
+
+			outputString += "\n-- [Value: " + to_string(valueDelivered)
+				+ " (weighted value: " + to_string(valueDelivered * sprint.sprintBonus) + "), "
+				+ "story points: " + to_string(storyPointsAssigned) + "]";
+
+			outputString += "\n\n";
+		}
+
+		return outputString;
 	}
 };
 
@@ -144,6 +409,32 @@ vector<string> splitString(const string& s, char delimiter) {
 	}
 
 	return tokens;
+}
+
+Roadmap greedyInsertStories(vector<Story> storiesToInsert, Roadmap roadmap) {
+	while (storiesToInsert.size() > 0) {
+		Story story = storiesToInsert[0];
+
+		// Greedily re-insert the story into a sprint
+		for (Sprint sprint : roadmap.sprints) {
+			if (sprint.sprintNumber == -1 || roadmap.validInsert(story, sprint)) {
+				roadmap.addStoryToSprint(story, sprint);
+				storiesToInsert.erase(remove(storiesToInsert.begin(), storiesToInsert.end(), story), storiesToInsert.end());
+
+				// Break out of traversing the sprints and move to the next story
+				break;
+			}
+		}
+	}
+
+	return roadmap;
+}
+
+Roadmap randomRoadmap(vector<Story> storyData, vector<Sprint> sprintData) {
+	vector<Story> shuffledStories = storyData;
+	random_shuffle(shuffledStories.begin(), shuffledStories.end());
+	Roadmap roadmap = greedyInsertStories(shuffledStories, Roadmap(storyData, sprintData));
+	return roadmap;
 }
 
 int main(int argc, char* argv[]) {
@@ -196,6 +487,12 @@ int main(int argc, char* argv[]) {
 				}
 			}
 		}
+
+		for (Story story : storyData) {
+			for (int dependency : story.dependencies) {
+				storyData[dependency].dependees.push_back(story.storyNumber);
+			}
+		}
 	}
 	else {
 		cout << "Cannot open story data file" << endl;
@@ -221,6 +518,8 @@ int main(int argc, char* argv[]) {
 
 			sprintData.push_back(Sprint(sprintNumber, sprintCapacity, sprintBonus));
 		}
+
+		sprintData.push_back(Sprint(-1, 0, 0)); // A special sprint representing 'unassigned' (i.e. assigned to the product backlog)
 	}
 	else {
 		cout << "Cannot open sprint data file" << endl;
@@ -228,6 +527,8 @@ int main(int argc, char* argv[]) {
 	}
 
 	sprintsFile.close();
+
+	auto t_solveStart = chrono::high_resolution_clock::now();
 
 	int numberOfStories = storyData.size();
 	int numberOfSprints = sprintData.size();
@@ -314,7 +615,38 @@ int main(int argc, char* argv[]) {
 		IloCplex cplex(model);
 		cplex.setOut(env.getNullStream());
 
-		auto t_solveStart = chrono::high_resolution_clock::now();
+		// CPLEX tuning
+		// http://www-01.ibm.com/support/docview.wss?uid=swg21400023#Item6
+
+		// Use MIP start to help the B&B by giving it a greedily-built solution
+		// https://www.ibm.com/support/knowledgecenter/SSSA5P_12.8.0/ilog.odms.cplex.help/CPLEX/OverviewAPIs/topics/MIP_starts.html
+
+		//////////////////////////////////////////////////////////////////////////
+
+		Roadmap warmStart = randomRoadmap(storyData, sprintData);
+
+		IloNumVarArray startVar(env);
+		IloNumArray startVal(env);
+
+		for (auto pair : warmStart.sprintToStories) {
+			Sprint sprint = pair.first;
+
+			if (sprint.sprintNumber != -1) {
+				vector<Story> stories = pair.second;
+
+				for (Story story : stories) {
+					startVar.add(roadmap[sprint.sprintNumber][story.storyNumber]);
+					startVal.add(1);
+				}
+			}
+		}
+
+		cplex.addMIPStart(startVar, startVal);
+
+		startVal.end();
+		startVar.end();
+		
+		//////////////////////////////////////////////////////////////////////////
 
 		if (cplex.solve()) {
 			auto t_solveEnd = chrono::high_resolution_clock::now();
@@ -323,56 +655,42 @@ int main(int argc, char* argv[]) {
 			int totalBusinessValueDelivered = 0;
 			int totalStoryPointsDelivered = 0;
 
-			if (false) {
-				/*cout << endl << "Product backlog: " << endl << endl;
+			// Output the solution
+			for (int i = 0; i < numberOfSprints; ++i) {
+				// Sprint information
+				cout << sprintData[i].toString() << endl;
 
-				for (int i = 0; i < storyData.size(); ++i) {
-					cout << storyData[i].toString() << endl;
-				}
+				int businessValueDelivered = 0;
+				int storyPointedDelivered = 0;
 
-				cout << endl << "Available sprints: " << endl << endl;
+				for (int j = 0; j < numberOfStories; ++j) {
+					// If the story was taken in this sprint, print the story's information
+					if (cplex.getValue(roadmap[i][j]) == 1) {
+						businessValueDelivered += storyData[j].businessValue;
+						storyPointedDelivered += storyData[j].storyPoints;
 
-				for (int i = 0; i < sprintData.size(); ++i) {
-					cout << sprintData[i].toString() << endl;
-				}*/
+						// Running totals for how much the roadmap delivered overall
+						totalBusinessValueDelivered += storyData[j].businessValue;
+						totalStoryPointsDelivered += storyData[j].storyPoints;
 
-				cout << endl << "---------------------------------------------------------" << endl << endl;
-
-				cout << "Sprint plan:" << endl << endl;
-
-				// Output the solution
-				for (int i = 0; i < numberOfSprints; ++i) {
-					// Sprint information
-					cout << sprintData[i].toString() << endl;
-					int businessValueDelivered = 0;
-					int storyPointedDelivered = 0;
-
-					for (int j = 0; j < numberOfStories; ++j) {
-						// If the story was taken in this sprint, print the story's information
-						if (cplex.getValue(roadmap[i][j]) == 1) {
-							businessValueDelivered += storyData[j].businessValue;
-							storyPointedDelivered += storyData[j].storyPoints;
-
-							// Running totals for how much the roadmap delivered overall
-							totalBusinessValueDelivered += storyData[j].businessValue;
-							totalStoryPointsDelivered += storyData[j].storyPoints;
-
-							cout << "\t" << storyData[j].toString() << endl;
-						}
+						cout << "\t" << storyData[j].toString() << endl;
 					}
-
-					// What was delivered in the sprint
-					cout << "-- [Value: " << to_string(businessValueDelivered)
-						<< " (weighted business value: " << to_string(businessValueDelivered * sprintData[i].sprintBonus) << "), "
-						<< "story points: " << to_string(storyPointedDelivered) << "]" << endl << endl;
 				}
+
+				// What was delivered in the sprint
+				cout << "-- [Value: " << to_string(businessValueDelivered)
+					<< " (weighted business value: " << to_string(businessValueDelivered * sprintData[i].sprintBonus) << "), "
+					<< "story points: " << to_string(storyPointedDelivered) << "]" << endl << endl;
 			}
 
-			cout << endl << "Solution status: " << cplex.getStatus() << endl;
+			cout << endl << cplex.getStatus() << endl;
 			cout << "Solved in " << chrono::duration<double, std::milli>(t_solveEnd - t_solveStart).count() << " ms" << endl << endl;
 			cout << "Stories: " << storyData.size() << ", sprints: " << sprintData.size() << endl;
 			cout << "Total weighted business value: " << cplex.getObjValue() << endl << endl;
-			cout << "--------------------------------------------------------------------------------------------------------" << endl;
+			cout << "----------------------------------------" << endl;
+
+			//cout << endl << storyData.size() << "," << sprintData.size() - 1 << "," << set << ",cold";
+			//cout << "," << cplex.getObjValue() << "," << chrono::duration<double, std::milli>(t_solveEnd - t_solveStart).count();
 		}
 		else {
 			cout << " No solution found" << endl;
